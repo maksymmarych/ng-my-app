95970e393568374f81540aea4d75a190f2e44029
maksymmarych <maksymmarych@gmail.com>
2018-02-27 10:06:45 +0200

    #*ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,13 +11,20 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
-  constructor() { }
+  constructor() {
+    let num: number = Math.random();
+    this.serverStatus = num > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit() {
   }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
